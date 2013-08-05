8763d86a758995d3512b2ac76c685853047b52bf
ebrnd <ebrnd@Zylon.localdomain>
2013-08-05 22:44:53 +0200

    didn't know those were THAT global...

--- modules/raumstatus.lua
+++ modules/raumstatus.lua
@@ -4,12 +4,12 @@ local pcre = require("rex_pcre")
 
 local interface = {
   construct = function(interval, net, chan, statusurl)
-    update_timeout = 60 * interval
-    netw = net
-    chann = chan
+    raumstatus_update_timeout = 60 * interval
+    raumstatus_netw = net
+    raumstatus_chann = chan
     url = statusurl
 
-    last_checked = 0
+    raumstatus_last_checked = 0
     last_raumstatus = ""
 
 		current_topic = ""
@@ -22,11 +22,11 @@ local interface = {
 
   step = function()
 		-- only check once every n minutes
-		if last_checked + update_timeout < os.time() then
-			last_checked = os.time()
+		if raumstatus_last_checked + raumstatus_update_timeout < os.time() then
+			raumstatus_last_checked = os.time()
 
 			if current_topic == "" then -- if we don't know the topic yet, send a request...
-				networks[netw].send("TOPIC", chann, "")
+				networks[raumstatus_netw].send("TOPIC", raumstatus_chann, "")
 			else -- ...and don't do anything else
 				-- check the raumstatus
 				local raumstatus = http.request(url)
@@ -39,8 +39,8 @@ local interface = {
 							local new_topic = pcre.gsub(current_topic,
 								"(?<=[{]).*?(?=[}])", raumstatus)
 							if not (current_topic == new_topic) then
-								networks[netw].send("TOPIC", chann, new_topic)
-								networks[netw].send("PRIVMSG", chann, "Der Raum ist nun " .. raumstatus)
+								networks[raumstatus_netw].send("TOPIC", raumstatus_chann, new_topic)
+								networks[raumstatus_netw].send("PRIVMSG", raumstatus_chann, "Der Raum ist nun " .. raumstatus)
 							end
 						end
 					last_raumstatus = raumstatus
@@ -51,24 +51,24 @@ local interface = {
 
   handlers = {
 		privmsg = function(network, sender, channel, message)
-			if (network == networks[netw]) and (channel == chann) then
+			if (network == networks[raumstatus_netw]) and (channel == raumstatus_chann) then
 				cmd = pcre.match(message, "!raumstatus")
 				if cmd then
-					networks[netw].send("PRIVMSG", chann, "Der raum ist gerade " .. last_raumstatus)
+					networks[raumstatus_netw].send("PRIVMSG", raumstatus_chann, "Der raum ist gerade " .. last_raumstatus)
 				end
 			end
     end,
 
 		topic = function(network, sender, channel, message)
-			if (network == networks[netw]) and (channel == chann) then
+			if (network == networks[raumstatus_netw]) and (channel == raumstatus_chann) then
 				-- keep track of topic changes so we can update the topic
 				current_topic = message
-				last_checked = 0 -- immediately check topic
+				raumstatus_last_checked = 0 -- immediately check topic
 			end
 		end,
 
 		[332] = function(network, code, nick, channel, top) -- replies to our own topic requests
-			if (network == networks[netw]) and (channel == chann) then
+			if (network == networks[raumstatus_netw]) and (channel == raumstatus_chann) then
 				current_topic = top
 			end
 		end
